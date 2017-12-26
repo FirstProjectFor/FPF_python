bf73a1c880ac36b7931a866edcd991091fe0938d
sunfielong1993 <1498282352@qq.com>
2017-12-26 16:36:05 +0800

    更新Readme

--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,6 @@
-Python Learn Record
+##
+
+Python Learn Code
 
 
 
--- readobject.py
+++ readobject.py
@@ -4,13 +4,16 @@ movies = ["老无所依", 1954,
           "藩篱", 1955,
           "岁月神偷", 1966,
           "盗梦空间", 1983]
+
 file = "data.txt"
 
 try:
     with open("data.txt", mode="wb") as out:
+        # 保存对象数据到文件
         pickle.dump(movies, file=out)
 
     with open("data.txt", mode="rb") as out:
+        # 从文件中读取对象
         movies_copy = pickle.load(out)
         print(movies_copy)
         print("movies_copy is list: " + str(isinstance(movies_copy, list)))
