4cbd026fd6e2fbbf03b1b65ce22ff6844d6279a6
sunfielong1993 <1498282352@qq.com>
2018-01-06 17:21:33 +0800

    修改测试demo

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -17,6 +17,7 @@ class TestDemo(unittest.TestCase):
     def tearDown(self):
         print("tearDown")
 
+    @unittest.expectedFailure
     def test_error(self):
         self.assertTrue(1 + 1 != 2, "1+1 != 2")
 
@@ -26,6 +27,14 @@ class TestDemo(unittest.TestCase):
     def test_method_second(self):
         self.assertTrue(1 + 2 == 3, "1+2 = 3")
 
+    def test_even(self):
+        for i in range(0, 4):
+            with self.subTest(i=i):
+                self.assertEqual(i % 2, 0)
+
     @unittest.skip("测试跳过")
     def test_skip(self):
         self.assertTrue(True, True)
+
+    def test_skip_in_code(self):
+        self.skipTest("测试代码内跳过测试")
