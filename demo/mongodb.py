401c01dd066a82d852bf47b7268861676db745f6
sunfielong1993 <1498282352@qq.com>
2018-01-16 22:46:41 +0800

    增加文件存储和认证

--- demo/mongodb.py
+++ demo/mongodb.py
@@ -1,9 +1,24 @@
+import gridfs
 import pymongo
 from pymongo import ReturnDocument
 
 client = pymongo.MongoClient("localhost", 27017)
 
 db = client.get_database("test")
+
+# admin 在命令行添加用户
+# db.createUser(
+#   {
+#     user: "test",
+#     pwd: "000000",
+#     roles: [
+#        { role: "readWrite", db: "test" }
+#     ]
+#   }
+# )
+
+# 认证
+db.authenticate(name="test", password="000000")
 
 # 插入测试数据
 if "test" in db.collection_names():
@@ -29,11 +44,20 @@ for i in range(10):
     before = test_collection.find_one_and_update(filter_temp, update_temp, return_document=ReturnDocument.BEFORE)
     print(before)
 
-#
-
-
 # 查询数据
 for name in db.collection_names():
     collection = db.get_collection(name)
     for document in collection.find().limit(10000).skip(0):
         print(document)
+
+# 存储文件
+gfs = gridfs.GridFS(db)
+with open("data.txt", mode="r", encoding="utf-8") as in_file:
+    if gfs.exists(document_or_id="1"):
+        gfs.delete(file_id="1")
+    file_id = gfs.put(in_file.read(), _id="1", filename="data.txt", encoding="utf-8")
+    print(gfs.list())
+    content = gfs.get(file_id).read()
+    print(content)
+
+client.close()
