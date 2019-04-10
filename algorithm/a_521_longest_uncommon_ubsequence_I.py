d509a62c713b975626a9e788ecea23c08c479d37
sunfeilong <sunfeilong1993@gmail.com>
2019-04-10 20:57:55 +0800

    leetcode 521

--- algorithm/a_521_longest_uncommon_ubsequence_I.py
+++ algorithm/a_521_longest_uncommon_ubsequence_I.py
@@ -1,13 +1,8 @@
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        result = -1
-        if len(a) != len(b):
-            result = max(len(a), len(b))
-        elif a.find(b) == -1:
-            result = len(a)
-        elif b.find(a) == -1:
-            result = len(b)
-        return result
+        if a == b:
+            return -1
+        return max(len(a), len(b))
 
 
 solution = Solution()
