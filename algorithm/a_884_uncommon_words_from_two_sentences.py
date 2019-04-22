8180a2505ddf3be147412c83c6e0f21902c0ea3f
sunfeilong <sunfeilong1993@gmail.com>
2019-04-22 22:28:14 +0800

    leetcode 884

--- algorithm/a_884_uncommon_words_from_two_sentences.py
+++ algorithm/a_884_uncommon_words_from_two_sentences.py
@@ -1,29 +1,20 @@
 class Solution:
     def uncommonFromSentences(self, A: str, B: str) -> list:
 
-        word_dict_a = {}
-        word_dict_b = {}
+        word_dict = {}
         result = []
 
-        for word in A.split():
-            if word in word_dict_a:
-                word_dict_a[word] += 1
+        words = A.split() + B.split()
+
+        for word in words:
+            if word in word_dict:
+                word_dict[word] += 1
             else:
-                word_dict_a[word] = 1
+                word_dict[word] = 1
 
-        for word in B.split():
-            if word in word_dict_b:
-                word_dict_b[word] += 1
-            else:
-                word_dict_b[word] = 1
-
-        for word in A.split():
-            if word_dict_a[word] == 1 and word not in word_dict_b:
-                result.append(word)
-
-        for word in B.split():
-            if word_dict_b[word] == 1 and word not in word_dict_a:
-                result.append(word)
+        for k, v in word_dict.items():
+            if v == 1:
+                result.append(k)
 
         return result
 
